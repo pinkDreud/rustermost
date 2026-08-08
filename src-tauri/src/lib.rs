03879972b63d5e158d426cc1abd5548541824861
Matteo Magherini <matteo.magherini@cern.ch>
2026-08-08 19:28:07 +0200

    update gitignore

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,11 +4,29 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn get_app_status() -> String {
+    "Backend Rust is on!".to_string()
+}
+
+#[tauri::command]
+async fn open_sso_window(app: tauri::AppHandle, url: String) -> Result<(), String> {
+    let parsed_url = url.parse::<tauri::Url>().map_err(|e| e.to_string())?;
+
+    tauri::WebviewWindowBuilder::new(&app, "sso-login", tauri::WebviewUrl::External(parsed_url))
+        .title("Login SSO")
+        .inner_size(500.0, 700.0)
+        .build()
+        .map_err(|e| e.to_string())?;
+
+    Ok(())
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, get_app_status, open_sso_window])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.js
+++ src/main.js
@@ -4,15 +4,27 @@ let greetInputEl;
 let greetMsgEl;
 
 async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
   greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
 }
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("DOMContentLoaded", async () => {
   greetInputEl = document.querySelector("#greet-input");
   greetMsgEl = document.querySelector("#greet-msg");
   document.querySelector("#greet-form").addEventListener("submit", (e) => {
     e.preventDefault();
     greet();
   });
+  const status = await invoke("get_app_status");
+  document.querySelector("#status").textContent = status;
+  document.querySelector("#url-form").addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const out = document.querySelector("#sso-result");
+    const url = document.querySelector("#url-input").value;
+    try {
+      await invoke("open_sso_window", { url });
+      out.textContent = "SSO window open"; 
+    } catch (err) {
+      out.textContent = "Error" + err;
+    } 
+  })
 });
